270ec0fd727ea0a55510b6d292446103236ac6c0
itsokto <picnet9@gmail.com>
2020-02-24 21:47:09 +0300

    Initial commit

---
+++ ddgo/TGBot.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace ddgo
+{
+	public class TGBot : IBot
+	{
+		/// <inheritdoc />
+		public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = new CancellationToken())
+		{
+			if (turnContext.Activity.Type == ActivityTypes.Message)
+			{
+				await turnContext.SendActivityAsync(turnContext.Activity.Text, cancellationToken: cancellationToken);
+			}
+		}
+	}
+}
