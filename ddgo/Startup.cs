9c82456aba13b5abaefbeee07f7005c11ad8d379
itsokto <picnet9@gmail.com>
2020-02-24 23:36:55 +0300

    Test

--- ddgo/Startup.cs
+++ ddgo/Startup.cs
@@ -21,7 +21,7 @@ namespace ddgo
 		public void ConfigureServices(IServiceCollection services)
 		{
 			//services.AddControllers();
-			services.AddBot<TGBot>(options => { options.CredentialProvider = new ConfigurationCredentialProvider(Configuration); });
+			services.AddBot<TGBot>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- ddgo/TGBot.cs
+++ ddgo/TGBot.cs
@@ -2,6 +2,10 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using Newtonsoft.Json.Linq;
+using Telegram.Bot.Requests;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InlineQueryResults;
 
 namespace ddgo
 {
@@ -10,9 +14,22 @@ namespace ddgo
 		/// <inheritdoc />
 		public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = new CancellationToken())
 		{
-			if (turnContext.Activity.Type == ActivityTypes.Message)
+			if (turnContext.Activity.ChannelId == "telegram")
 			{
-				await turnContext.SendActivityAsync(turnContext.Activity.Text, cancellationToken: cancellationToken);
+				var inlineQuery = turnContext.Activity.GetChannelData<InlineQuery>();
+
+				var answer =
+					new JObject(new JProperty("method", "answerInlineQuery"),
+								new JProperty("parameter", new AnswerInlineQueryRequest(inlineQuery.Id, new[]
+								{
+									new InlineQueryResultPhoto("0",
+															   "https://klike.net/uploads/posts/2019-03/1551516106_1.jpg",
+															   "https://klike.net/uploads/posts/2019-03/1551516106_1.jpg"),
+								})));
+
+				var reply = turnContext.Activity.CreateReply();
+				reply.ChannelData = answer;
+				await turnContext.SendActivityAsync(reply, cancellationToken);
 			}
 		}
 	}
